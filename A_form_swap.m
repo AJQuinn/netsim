60ea94075efde089ff0d8380858ff81e74051c02
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2016-07-30 12:18:45 +0100

    Add extra parameter forms to form swap

--- A_form_swap.m
+++ A_form_swap.m
@@ -1,8 +1,10 @@
-function [ Anew ] = A_form_swap( A, nnodes, order )
+function [ Anew ] = A_form_swap( A, method, nnodes, order )
 %% function [ Anew ] = A_form_swap( A, nnodes, order )
 %
 % Convert autoregressive parameter matrix between 3D typical form and 2D
-% companion form
+% companion form.
+%
+% forms - comp2full, stack2comp, full2comp
 %
 % Input
 %   A - autoregressive parameters
@@ -10,18 +12,28 @@ function [ Anew ] = A_form_swap( A, nnodes, order )
 %   order - number of time lags in model
 
 
-if ndims(A) == 2
+if strcmp(method,'comp2full')
 
     % Change to 3d form
     Anew = reshape(A(1:nnodes,:),nnodes,nnodes,[]);
     Anew = cat(3,eye(nnodes),Anew);
 
-else
+elseif strcmp(method,'stack2comp')
+
+    % Change to companion
+    Anew = zeros(nnodes*(order),nnodes*(order));
+    Anew(1:nnodes,:) = A;
+    Anew(nnodes+1:end,1:end-nnodes) = eye(-nnodes + (order)*nnodes);
+
+elseif strcmp(method,'full2comp')
 
     % Change to companion
     Anew = zeros(nnodes*(order),nnodes*(order));
     Anew(1:nnodes,:) = reshape(A(:,:,2:end),nnodes,[]);
     Anew(nnodes+1:end,1:end-nnodes) = eye(-nnodes + (order)*nnodes);
+
+else
+    error('method not recognised');
 
 end
 
